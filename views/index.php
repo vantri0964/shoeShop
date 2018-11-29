a80241ed5cadf429bb61448d295f344ab2449be8
ntphuc98 <45426769+ntphuc98@users.noreply.github.com>
2018-11-29 19:51:30 +0700

    MVC
    Tổ chứ mvc

---
+++ views/index.php
@@ -0,0 +1,272 @@
+
+<?php 
+	require_once('./header.php');
+ ?>
+ 	<!-- banner -->
+	<div class="banner">
+		<div class="banner-matter">
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  				<ol class="carousel-indicators">
+				    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				    <li data-target="#myCarousel" data-slide-to="1"></li>
+				    <li data-target="#myCarousel" data-slide-to="2"></li>
+				</ol>
+  				<div class="carousel-inner">
+				    <div class="item active">
+				      	<img src="../images/banner.jpg" alt="Chania">
+				      	<div class="carousel-caption">
+				        	<h3>Los Angeles</h3>
+				        	<p>LA is always so much fun!</p>
+				      	</div>
+				    </div>
+    				<div class="item">
+      					<img src="../images/banner.jpg" alt="Chicago">
+      					<div class="carousel-caption">
+        				<h3>Chicago</h3>
+        				<p>Thank you, Chicago!</p>
+      					</div>
+    				</div>
+    				<div class="item">
+      					<img src="../images/banner.jpg" alt="New York">
+      					<div class="carousel-caption">
+        				<h3>New York</h3>
+        				<p>We love the Big Apple!</p>
+      					</div>
+    				</div>
+  				</div>
+  				<!-- Left and right controls -->
+  				<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+				    <span class="glyphicon glyphicon-chevron-left"></span>
+				    <span class="sr-only">Previous</span>
+				</a>
+  				<a class="right carousel-control" href="#myCarousel" data-slide="next">
+    				<span class="glyphicon glyphicon-chevron-right"></span>
+    				<span class="sr-only">Next</span>
+  				</a>
+			</div>
+		</div>	
+	</div>
+	<!-- end banner -->
+	<!--content-->
+	<div class="content">
+				<div class="col-md-12" id="title-product">
+	        <h2 align="center"><b>Sản Phẩm</b></h2>
+
+			</div>
+
+			<!--//sreen-gallery-cursual---->
+			<div class="content-grids">
+
+				<div class="product-top">
+					<!-- start-product -->
+					<div class="col-md-8" id="content-8">
+						<div class="col-md-6 grid-product-in">	
+							<div class=" product-grid">	
+								<a href="../single.html"><img class="img-responsive " src="../images/pr.png" alt=""></a>		
+								<div class="shoe-in">
+									<h6><a href="single.html">Lorem Ipsum is simply dummy </a></h6>
+									<label>$67.99</label>
+									<a href="single.html" class="store">Xem chi tiết</a>
+								</div>
+
+								<b class="plus-on">+</b>
+							</div>	
+						</div>
+						<div class="col-md-6 grid-product-in">	
+							<div class=" product-grid">	
+								<a href="single.html"><img class="img-responsive " src="../images/pr1.png" alt=""></a>
+								<div class="shoe-in">
+									<h6><a href="single.html">Lorem Ipsum is simply dummy </a></h6>
+									<label>$67.99</label>
+									<a href="single.html" class="store">Xem chi tiết</a>
+								</div>
+
+								<b class="plus-on">+</b>
+							</div>
+						</div>
+						<div class="col-md-6 grid-product-in">	
+							<div class=" product-grid">	
+								<a href="single.html"><img class="img-responsive " src="../images/pr2.png" alt=""></a>
+								<div class="shoe-in">
+									<h6><a href="single.html">Lorem Ipsum is simply dummy </a></h6>
+									<label>$67.99</label>
+									<a href="single.html" class="store">Xem chi tiết</a>
+								</div>
+
+								<b class="plus-on">+</b>
+							</div>
+						</div>
+						<div class="col-md-6 grid-product-in">	
+							<div class=" product-grid">	
+								<a href="single.html"><img class="img-responsive " src="../images/pr4.png" alt=""></a>
+								<div class="shoe-in">
+									<h6><a href="single.html">Lorem Ipsum is simply dummy </a></h6>
+									<label>$67.99</label>
+									<a href="single.html" class="store">Xem chi tiết</a>
+								</div>
+
+								<b class="plus-on">+</b>
+							</div>
+						</div>
+					</div>
+					<!-- end product -->
+					<div class="col-md-3 col-md-offset-1">
+					
+					<!-- <div class="col-md-10">
+						 <div class="jumbotron1 col-md-12">
+						 	<div id="jum1">
+						 	 	<center><B>SẢN PHẨM MỚI NHẤT</B></center>
+						 	 </div> 
+						 	<div>
+						 		<img src="images/pic.jpg" alt="" class="img-rounded">
+						 	</div>
+							
+						 </div>
+						 
+						 
+					</div> -->
+					 <section  class="sky-form">
+					<div class="sellers">
+							<h3 class="m_2">Special Offers</h3>
+							<section class="slider">
+						<div class="flexslider">
+							<ul class="slides">
+								<li>
+									<div class="tittle">
+										<img src="../images/si1.jpg" class="img-responsive" alt="" height="150px"  />
+										<h6>Item Title Here</h6>
+										<p>Lorem ipsum dolor sit amet,</p>
+										<p>Lorem ipsum dolor sit amet,</p>
+										<p>Lorem ipsum dolor sit amet,</p>
+										<a class="show1" href="#">SHOW ME MORE</a>
+									</div>
+								</li>
+								<li>
+									<div class="tittle">
+										<img src="../images/si2.jpg" class="img-responsive" alt=""/>
+										<h6>Item Title Here</h6>
+										<p>Lorem ipsum dolor sit amet,</p>
+										<p>Lorem ipsum dolor sit amet,</p>
+										<p>Lorem ipsum dolor sit amet,</p>
+										<a class="show1" href="#">SHOW ME MORE</a>
+									</div>
+								</li>
+								<li>	
+									<div class="tittle">
+										<img src="../images/si1.jpg" class="img-responsive" alt=""/>
+										<h6>Item Title Here</h6>
+										<p>Lorem ipsum dolor sit amet,</p>
+										<p>Lorem ipsum dolor sit amet,</p>
+										<p>Lorem ipsum dolor sit amet,</p>
+										<a class="show1" href="#">SHOW ME MORE</a>
+									</div>
+								</li>
+								<li>	
+									<div class="tittle">
+										<img src="../images/si3.jpg" class="img-responsive" alt=""/>
+										<h6>Item Title Here</h6>
+										<p>Lorem ipsum dolor sit amet,</p>
+										<p>Lorem ipsum dolor sit amet,</p>
+										<p>Lorem ipsum dolor sit amet,</p>
+										<a class="show1" href="#">SHOW ME MORE</a>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</section>
+				
+						<!-- FlexSlider -->
+							  <script defer src="../js/jquery.flexslider.js"></script>
+							  <script type="text/javascript">
+								$(function(){
+								  SyntaxHighlighter.all();
+								});
+								$(window).load(function(){
+								  $('.flexslider').flexslider({
+									animation: "slide",
+									start: function(slider){
+									  $('body').removeClass('loading');
+									}
+								  });
+								});
+							  </script>
+						<!-- FlexSlider -->
+
+						</div>
+		       </section>
+					
+					</div>
+
+					<div class="clearfix"> </div>
+				</div>	
+				<div class="clearfix"> </div>
+			</div>
+			<!---->
+
+
+		</div>	
+		<!--footer-->
+		<div class="footer">
+			<div class="col-md-4 footer-left">
+				<h4>Thành viên</h4>
+				<div class="run-top">
+					<ul class="run-grid">
+						<li><a href="product.html">+ UNG THỊ THÙY OANH</a></li>
+						<li><a href="product.html">+ TRẦN THỊ CẨM NGA</a></li>
+						<li><a href="product.html">+ LÊ VĂN TRÍ</a></li>
+						<li><a href="product.html">+ NGUYỄN THANH PHÚC</a></li>
+						<li><a href="product.html">+ UNG NHO TIÊN</a></li>
+					</ul>
+					<ul class="run-grid">
+						<li><a href="product.html">16CNTT3</a></li>
+						<li><a href="product.html">16CNTT3</a></li>
+						<li><a href="product.html">16CNTT3</a></li>
+						<li><a href="product.html">16CNTT3</a></li>
+						<li><a href="product.html">16CNTT3</a></li>
+					</ul>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="col-md-4 footer-left left-footer">
+				<h4>THÔNG TIN</h4>
+				<div class="run-top top-run">
+					<ul class="run-grid">
+						<li><a href="#">Giày Nam</a></li>
+						<li><a href="#">Giày Nữ</a></li>
+						<li><a href="#">Giới thiệu</a></li>
+						<li><a href="#">Giá:Thấp đến Cao</a></li>
+						<li><a href="#">Giá:Cao đến Thấp</a></li>
+
+					</ul>
+					
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			
+			<div class="col-md-4 footer-left left-footer">
+				<ul class="social-in">
+					<li><a href="#"><i> </i></a></li>
+					<li><a href="#"><i class="youtube"> </i></a></li>
+					<li><a href="#"><i class="facebook"> </i></a></li>
+					<li><a href="#"><i class="twitter"> </i></a></li>
+				</ul>
+				<div class="letter run-top">
+					<h4> Góp Ý </h4>
+				<div class="cont-1" >
+					<form>
+						<input type="text" class="textbox" value="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}">
+						<input type="text" class="textbox" value="Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email';}">
+						<textarea value="Message:" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Message';}">Message..</textarea>
+						 <div class="smt">
+							<input type="submit" value="send">
+						 </div>
+					</form>
+			      </div>
+					<p class="footer-class"> © 2018 Shoe Shop | Nhóm </p>
+				</div>
+
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</body>
+	</html>
